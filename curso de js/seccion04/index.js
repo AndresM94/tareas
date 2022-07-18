80f76e5c5ab53ec93bdbfbf8d9ef8b537f651ff4
AndresM9406 <87483102+AndresM94@users.noreply.github.com>
2022-07-18 15:57:46 -0500

    Create index.js

---
+++ curso de js/seccion04/index.js
@@ -0,0 +1,8 @@
+let nombre = "Andres";
+let apellido = "Marin";
+let estudiante = nombre.concat(" ", apellido);
+let estudianteMayus = estudiante.toUpperCase();
+let estudainteMinus = estudiante.toLowerCase();
+let numeroLetras = estudiante.length;
+let primeraLetra = estudiante.charAt(0);
+let ultimaLetra = estudiante.charAt(11);
