5ad9a2f266d480cf538886a647a7fb7f1dd1be08
AndresM9406 <87483102+AndresM94@users.noreply.github.com>
2022-07-19 16:48:42 -0500

    Create index.js

---
+++ curso de js/seccion05/index.js
@@ -0,0 +1,11 @@
+const alturaInt = 181;
+
+const alturaFloat = 1.81;
+
+const pesoKg = 57.4;
+
+const alturaRedondeado = Math.ceil(alturaFloat);
+
+const pesoRedeondeado = Math.floor(pesoKg);
+
+const iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
