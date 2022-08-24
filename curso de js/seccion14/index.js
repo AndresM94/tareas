28ddb57a38119aeb8c6cd13c362b7df515a58d48
AndresM9406 <87483102+AndresM94@users.noreply.github.com>
2022-08-24 18:37:41 -0500

    Create index.js

---
+++ curso de js/seccion14/index.js
@@ -0,0 +1,7 @@
+let boton = document.querySelector("#btn-inicio")
+
+boton.addEventListener('click', ()=> alert('click en el botón'))
+
+$("button").click(function() {
+    console.log("Hola, estoy utilizando jQuery")
+})
